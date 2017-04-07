90ec77686f3ee8004d3e7d489df44c19c7b3f545
Alejandro Mdz <alejandromdz@gmail.com>
2017-04-06 21:17:41 -0700

    multiple dropdown

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-const ul=document.querySelector('[data-dropdown-item]');
+const lis = document.querySelectorAll('[data-dropdown-item]');
 
-ul.addEventListener('click',function(ev){
-    const target=ev.target;
-    ev.stopPropagation();    
-    if(target.dataset){
-        if(target.dataset.dropdownItem!==undefined)
-        {
-            target.classList.toggle('active');
+lis.forEach(li => {
+    li.addEventListener('click', function (ev) {
+        const target = ev.target;
+        if(!target.classList.contains('active')){
+        const activeElements = document.querySelectorAll('[data-dropdown-item].active');
+        activeElements.forEach(elem => { elem.classList.remove('active') })
         }
-    }
-})
-
-window.onclick = function(e) {
-  const activeElements=document.querySelectorAll('[data-dropdown-item].active');
-  activeElements.forEach(function(elem){
-      elem.classList.remove('active')
-  })
+        ev.stopPropagation();
+        if (target.dataset) {
+            if (target.dataset.dropdownItem !== undefined) {
+                target.classList.toggle('active');
+            }
+        }
+    })
+});
+window.onclick = function (e) {
+    const activeElements = document.querySelectorAll('[data-dropdown-item].active');
+    activeElements.forEach(elem => { elem.classList.remove('active') })
 }
